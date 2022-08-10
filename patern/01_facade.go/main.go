165f20517ed3c0737392acf3b4b84e8bd642e7a1
MrDavudov <davruslan90@gmail.com>
2022-08-10 20:51:56 +0300

    updata

---
+++ patern/01_facade.go/main.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+Реализовать паттерн «фасад».
+Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
+https://en.wikipedia.org/wiki/Facade_pattern
+*/
+
+import (
+	"fmt"
+)
+
+var (
+	bank = pgk.Bank {
+		Name: "БАнк",
+		Cards: []pkg.Card{},
+	}
+)
+
+func main() {
+	
+}
